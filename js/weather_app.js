32942c1e52d37da2590b366f0301ba199df9eb99
mtodorov95 <mtodorov95@gmail.com>
2020-08-01 19:02:41 +0300

    5-day forecast

--- js/weather_app.js
+++ js/weather_app.js
@@ -70,7 +70,7 @@ function createDay(day, to_add){
 
 function dateBuilder(d){
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     let day = days[d.getDay()];
     let date = d.getDate();
